4ef49b591fe2542028c944711e35913c23146721
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-28 00:12:30 +0800

    successful build for the interface

---
+++ src/EquipmentBorrowing.Application/Interfaces/IBorrowingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+using EquipmentBorrowing.Domain;
+namespace EquipmentBorrowing.Application.Interfaces;
+
+public interface IBorrowingRepository
+{
+    Task<IReadOnlyList<Borrowing>> getActiveByStudentIdAsync(Guid studentId);
+    Task Addsync(Borrowing borrowing);
+}
