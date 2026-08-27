37726051fef44b4fb6524f6c7b2d376d67eac42f
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-27 23:18:34 +0800

    equipment class created and build

---
+++ src/EquipmentBorrowing.Domain/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Domain;
+
+public class Equipment
+{
+    public Guid EquipmentId { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public bool IsAvailable { get; private set; }
+
+
+    public Equipment(Guid equipmentId, string name, string description, bool isAvailable)
+    {
+        EquipmentId = equipmentId;
+        Name = name;
+        Description = description;
+        IsAvailable = isAvailable;
+    }
+}
