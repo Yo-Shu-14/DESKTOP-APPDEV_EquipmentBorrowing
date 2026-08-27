34f7cd614a664ae3bfec2bffce58cd802b37baa8
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-27 23:33:05 +0800

    created Borrowing class

---
+++ src/EquipmentBorrowing.Domain/Borrowing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Domain;
+public class Borrowing
+{
+    public Guid BorrowingId { get; private set; }
+    public Student Student { get; private set; }
+    public Equipment Equipment { get; private set; }
+
+    public Borrowing(Guid borrowingId, Student student, Equipment equipment)
+    {
+        BorrowingId = borrowingId;
+        Student = student;
+        Equipment = equipment;
+    }
+}
+
