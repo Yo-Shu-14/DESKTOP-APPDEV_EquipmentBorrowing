c7f4e3702cbdfd904f56f46715e33e31890ab7c2
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-27 23:05:47 +0800

    Student class created and bbuild

---
+++ src/EquipmentBorrowing.Domain/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Domain;
+
+public class Student
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public bool IsAllowedToBorrow { get; private set; }
+
+
+    //constructor
+    public Student(int Id, string Name, bool IsAllowedToBorrow)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.IsAllowedToBorrow = IsAllowedToBorrow;
+    }
+}
