524a93d85959347daf32b518b0c9026343ea46f0
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-28 00:04:42 +0800

    created studentinterface and equipmentinterface

---
+++ src/EquipmentBorrowing.Application/Interfaces/IEquipmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EquipmentBorrowing.Domain;
+namespace EquipmentBorrowing.Application.Interfaces;
+
+public interface IEquipmentRepository
+{
+    Task<Equipment?> GetByIdAsync(Guid id);
+}
---
+++ src/EquipmentBorrowing.Application/Interfaces/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EquipmentBorrowing.Domain;
+namespace EquipmentBorrowing.Application.Interfaces;
+
+public interface IStudentRepository
+{
+    Task<Student?> GetStudentByIdAsync(int studentId);
+}
