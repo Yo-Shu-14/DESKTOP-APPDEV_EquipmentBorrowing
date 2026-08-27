4c5b4cc315378dfc4df5f7be4131dc9943f31101
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-28 07:39:58 +0800

    Inmemorystudentrepo created and build

---
+++ src/EquipmentBorrowing.Infrastructure/InMemoryStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+namespace EquipmentBorrowing.Infrastructure;
+
+public class InMemoryStudentRepository : IStudentRepository
+{
+    private readonly List<Student> _students = new();
+
+    public Task<Student?> GetByIdAsync(int id)
+    {
+        var student = _students.FirstOrDefault(student => student.Id == id);
+
+        return Task.FromResult(student);
+    }
+}
+
