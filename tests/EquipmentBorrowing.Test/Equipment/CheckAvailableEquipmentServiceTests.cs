3745fc37f7e5eafae15b047dbcf55c8cf17fc5dd
MJ Jade G. Piquero <mjjadegpiquero@gmail.com>
2026-08-28 09:42:47 +0800

    Added test files for borrowing, returning, and checking equipment services

---
+++ tests/EquipmentBorrowing.Test/Equipment/CheckAvailableEquipmentServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Test.Equipment
+{
+    internal class CheckAvailableEquipmentServiceTests
+    {
+    }
+}
---
+++ tests/EquipmentBorrowing.Test/Returning/ReturnEquipmentServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentBorrowing.Test.Returning
+{
+    internal class ReturnEquipmentServiceTests
+    {
+    }
+}
