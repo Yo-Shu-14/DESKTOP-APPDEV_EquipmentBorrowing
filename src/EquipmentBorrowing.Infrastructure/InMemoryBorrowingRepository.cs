4e40c76ac7d4f2ae4b069955531ee2a2c1cf4315
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-08-28 08:04:42 +0800

    created repository for the borrowing

---
+++ src/EquipmentBorrowing.Infrastructure/InMemoryBorrowingRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EquipmentBorrowing.Application.Interfaces;
+using EquipmentBorrowing.Domain;
+namespace EquipmentBorrowing.Infrastructure;
+
+internal class InMemoryBorrowingRepository 
+{
+
+}
