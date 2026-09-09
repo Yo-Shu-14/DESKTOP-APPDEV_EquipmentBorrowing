838bdd96b448ca9d878ebb4c7901a050cd430a1d
Ye-Xiuu <magnojohncarlo0314@gmail.com>
2026-09-10 07:06:19 +0800

    Update console program

--- src/EquipmentBorrowing.Console/Program.cs
+++ src/EquipmentBorrowing.Console/Program.cs
@@ -2,9 +2,4 @@
 using System.Collections.Generic;
 using System.Text;
 
-<<<<<<< HEAD
 BorrowEquipmentSuccessCase.Run();
-=======
-//BorrowEquipmentSuccessCase.Run();
-BorrowEquipmentFailedCase.Run();
->>>>>>> 40c18376228a3d66649f3b1d49eaaa21d614be7d
